16c44a05b7052f17ff1d7ca440812710df5631fb
Meme1sh_Fetish <60296686+The-GitHub-Paradox@users.noreply.github.com>
2020-06-06 15:03:14 -0700

    yes
    yes

--- src/_02_methods/_6_slippery_dot/slippery_dot/slippery_dot.pde
+++ src/_02_methods/_6_slippery_dot/slippery_dot/slippery_dot.pde
@@ -26,7 +26,8 @@ int z = 50;
     /******** This method gets called automatically when you press the mouse ************/
     void mousePressed() {
       //5. Create an integer variable called distance
-      int distance = 0;
+      int distance = getDistanceFromMouse(x, y);
+      
       //6. The getDistanceFromMouse() method below returns a number.
       //   Set the value of your distance variable to the value returned by the getDistance method
       //   You will need to pass the x and y location of your ellipse to this method.
@@ -35,7 +36,10 @@ int z = 50;
       //7.  Use an if statement to check if your distance variable is < the radius of the ellipse
       //    If it is, make a new x AND y for the ellipse, for a new random location on the window
       //    Hint: Use code like this      x = (int) random(width);
-       
+      if(distance < z/2){
+       x = (int) random(500);
+       y = (int) random(500);
+      }
       
     }
     
--- src/_03_string_conversion/_3_rollercoaster/RollerCoaster.java
+++ src/_03_string_conversion/_3_rollercoaster/RollerCoaster.java
@@ -5,12 +5,21 @@ import javax.swing.JOptionPane;
 public class RollerCoaster {
 
 	// 1) Make a main method that includes all the steps below…. 
-		
+	public static void main(String[] args) {
 		// 2) Ask the user to enter their height in inches using JOptionPane and set it to the variable heightToRide
+		String input = JOptionPane.showInputDialog("How old are you?");
+		int heightToRide = Integer.parseInt(input);
 		
 		// Uncomment the line below...
-			//if( heightToRide > 42 ) { }
-		
+		if(heightToRide > 48) { 
+			System.out.print("Hurray! You are tall enough to ride the coaster alone!");
+		}
+		if(heightToRide >= 42) {
+			System.out.print("You can ride the coaster with someone else!");
+		}	
+		else {
+			System.out.print("You must be at least 42 inches tall to ride the roller coaster pal!");
+		}
 		// Do you see the heightToRide > 42 underlined red? That is because you are comparing a STRING to an INTEGER.
 		
 		// 3) Convert heightToRide to heightAsInt using Integer.parseInt();  Hint: int heightAsInt = Integer.parse... 
@@ -22,4 +31,5 @@ public class RollerCoaster {
 		// "You can ride the coaster with someone else!" 
 	
 		// 6) else print: "You must be at least 42 inches tall to ride the roller coaster pal!"
+	}
 } 
